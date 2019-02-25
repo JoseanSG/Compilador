09f1fd510046a49f1544ec0efcbfae1ce6f8a34e
JoseanSG <jantoniosg98@gmail.com>
2019-02-24 19:47:43 -0600

    jalando_al_100

--- Automatas/lexico.py
+++ Automatas/lexico.py
@@ -1,32 +1,92 @@
-texto = "qwuepi qwieu oqiqwe + + + +qwe + 123 141"
+import re
 
-tot_digitos = len(texto)
+f = open('CodigoMamalon.txt','r')
+code = f.read()
+f.close()
+
+tot_digitos = len(code)
 cont = 0
+line = 1
+tokens = []
 
-while cont < tot_digitos :
+class Token:
+	def __init__(self, tipo, lexema):
+		self.tipo = tipo
+		self.lexema = lexema
+	def print(self):
+		return self.tipo + " " + self.lexema
+		
+while cont  < tot_digitos:
 	buff = ""
+	if code[cont] == "(":
+		token = Token("simbolo_de_(","(")
+		print(token.print())
+		tokens.append(token)
 
-	if (ord('a') <= ord(texto[cont]) and ord(texto[cont]) <= ord('z')) or (ord("0") <= ord(texto[cont]) and ord(texto[cont]) <= ord("9")):
-		while (ord('a') <= ord(texto[cont]) and ord(texto[cont]) <= ord('z')) or (ord("0") <= ord(texto[cont]) and ord(texto[cont]) <= ord("9")):
-			buff = buff + texto[cont]
-			cont = cont + 1;
+	elif code[cont] == ")": 
+		token = Token("simbolo_de_)",")")
+		print(token.print())
+		tokens.append(token)
+
+	elif code[cont] == "==":
+		token = Token("simbolo_de_comparacion","==")
+		print(token.print())
+		tokens.append(token)
+
+	elif code[cont] =="=":
+		token = Token("simbolo_de_asignacion","=")
+		print(token.print())
+		tokens.append(token)
+
+	elif code[cont] == "+":
+		token = Token("simbolo_de_+","+")
+		print(token.print())
+		tokens.append(token)
+
+	elif code[cont] == '"':
+		token = Token('simbolo_de_"','"')
+		print(token.print())
+		tokens.append(token)
+
+	elif re.match('\w', code[cont]):
+
+		while re.match('\w', code[cont]):
 			if cont == (tot_digitos - 1):
-				buff = buff + texto[cont]
 				break
-		print("La palabra encontrada es",buff)
-		if cont == (tot_digitos - 1):
-			break
+			buff = buff + code[cont]
+			cont = cont + 1
 
-	elif texto[cont] == "+":
-		print("Suma")
-		cont = cont + 1
-		if cont == (tot_digitos - 1):
-			buff = buff + texto[cont]
-			break
+		if buff =="Begin":
+			token = Token("palabra_reservada","Begin")
+			print(token.print())
+			tokens.append(token)
+			cont = cont + 1
 
-	elif texto[cont] == " ":
-		print("Espacio")
-		cont = cont + 1
-		if cont == (tot_digitos - 1):
-			buff = buff + texto[cont]
-			break
+		elif buff == "Nucleo":
+			token = Token("palabra_reservada","Nucleo")
+			print(token.print())
+			tokens.append(token)
+			cont = cont + 1
+
+		elif buff == "Si":
+			token = Token("palabra_reservada","Si")
+			print(token.print())
+			tokens.append(token)
+			cont = cont + 1
+
+		elif buff == "Imprimir":
+			token = Token("palabra_reservada","Imprimir")
+			print(token.print())
+			tokens.append(token)
+			cont = cont + 1
+
+		elif buff == "Fin":
+			token = Token("palabra_reservada","Fin")
+			print(token.print())
+			tokens.append(token)
+			cont = cont + 1
+	# cont = cont + 1
+print(len(tokens))
+# patron = re.compile(r'\W+')
+# palabras = patron.split(code)
+# print(palabras)
--- Automatas/pruebas.py
+++ Automatas/pruebas.py
@@ -1,7 +1,23 @@
-print(ord('a'))
-print(ord('z'))
-print(ord('q'))
-print(ord('d'))
-print(ord('e'))
-print(ord('f'))
-print(ord("g"))
+import re
+f = open('CodigoMamalon.txt','r')
+code = f.read()
+print(code)
+f.close()
+
+tot_digitos = len(code)
+cont = 0
+line = 1
+tokens = {}
+
+while cont  < tot_digitos:
+	if re.match('\w', code[cont]):
+		print(code[cont])
+		cont = cont + 1
+	else:
+		cont = cont + 1
+	# var = code[cont]
+	# if var == "\n":
+	# 	print(line)
+	# 	line = line + 1 
+	# 	print("salto")
+	# cont = cont + 1
